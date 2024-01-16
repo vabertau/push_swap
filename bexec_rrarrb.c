0f9703f8a5f1b3dfa2275a6a50a4f7f749049b00
Valentin Bertaud <vabertau@bess-f3r5s2.clusters.42paris.fr>
2024-01-16 18:41:43 +0100

    doing norm b_exec_rrarrb working

--- bexec_rrarrb.c
+++ bexec_rrarrb.c
@@ -6,34 +6,29 @@
 /*   By: vabertau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 18:21:26 by vabertau          #+#    #+#             */
-/*   Updated: 2024/01/16 18:26:53 by vabertau         ###   ########.fr       */
+/*   Updated: 2024/01/16 18:40:54 by vabertau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int     bexec_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
+int     bindex00_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
 {
-        int     i;
-        swp_list        *tail_a;
-        swp_list        *tail_b;
-        int             rev_indexa;
-        int             rev_indexb;
-
-        tail_a = *a;
-        tail_b = *b;
-        i = 0;
-        while (tail_a->next)
-                tail_a = tail_a->next;
-        while (tail_b->next)
-                tail_b = tail_b->next;
-        rev_indexa = tail_a->index - (*to_push)->target->index;
-        rev_indexb = tail_b->index - (*to_push)->index;
         if ((*to_push)->index == 0 && (*to_push)->target->index == 0)
         {
                 pa(a, b);
-                return (0);
+                return (1);
         }
+        return (0);
+}
+
+int     bindex01_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
+{
+	int	i;
+        int             rev_indexa;
+
+        i = 0;
+        rev_indexa = get_revindex(a, &((*to_push)->target));
         if ((*to_push)->index == 0)
         {
                 while (i < rev_indexa + 1)
@@ -42,8 +37,18 @@ int     bexec_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
                         i++;
                 }
                 pa(a, b);
-                return (0);
+                return (1);
         }
+	return (0);
+}
+
+int     bindex10_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
+{
+	int	i;
+        int             rev_indexb;
+
+        rev_indexb = get_revindex(b, to_push);
+	i = 0;
         if ((*to_push)->target->index == 0)
         {
                 while (i < rev_indexb + 1)
@@ -52,8 +57,20 @@ int     bexec_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
                         i++;
                 }
                 pa(a, b);
-                return (0);
+                return (1);
         }
+	return(0);
+}
+
+int     bindex11_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
+{
+        int     i;
+        int             rev_indexa;
+        int             rev_indexb;
+
+        i = 0;
+        rev_indexa = get_revindex(a, &((*to_push)->target));
+        rev_indexb = get_revindex(b, to_push);
         if (bpc_rrarrb(*a, *to_push, *b) == rev_indexa + 1)
         {
                 while (i < rev_indexb + 1)
@@ -81,6 +98,77 @@ int     bexec_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
                 }
         }
         pa(a, b);
+        return (1);
+}
+
+int     bexec_rrarrb(swp_list **a, swp_list **to_push, swp_list **b)
+{
+        int     i;
+        int             rev_indexa;
+        int             rev_indexb;
+
+        i = 0;
+        rev_indexa = get_revindex(a, &((*to_push)->target));
+        rev_indexb = get_revindex(b, to_push);
+        /*if ((*to_push)->index == 0 && (*to_push)->target->index == 0)
+        {
+                pa(a, b);
+                return (0);
+        }*/
+        if (bindex00_rrarrb(a, to_push, b))
+                return (0);
+        if (bindex01_rrarrb(a, to_push, b))
+                return (0);
+        /*if ((*to_push)->index == 0)
+        {
+                while (i < rev_indexa + 1)
+                {
+                        rrab(a, 1);
+                        i++;
+                }
+                pa(a, b);
+                return (0);
+        }*/
+        /*if ((*to_push)->target->index == 0)
+        {
+                while (i < rev_indexb + 1)
+                {
+                        rrab(b, 2);
+                        i++;
+                }
+                pa(a, b);
+                return (0);
+        }*/
+        if (bindex10_rrarrb(a, to_push, b))
+                return (0);
+        /*if (bpc_rrarrb(*a, *to_push, *b) == rev_indexa + 1)
+        {
+                while (i < rev_indexb + 1)
+                {
+                        rrr(a, b);
+                        i++;
+                }
+                while (i < rev_indexa + 1)
+                {
+                        rrab(a, 1);
+                        i++;
+                }
+        }
+        else
+        {
+                while (i < rev_indexa + 1)
+                {
+                        rrr(a, b);
+                        i++;
+                }
+                while (i < rev_indexb + 1)
+                {
+                        rrab(b, 2);
+                        i++;
+                }
+        }
+        pa(a, b);*/
+	bindex11_rrarrb(a, to_push, b);
         return (0);
 }
 
